92237b3705c2082d0640554d3233d7dccd4ee522
Khanh <Khanh13Nguyen13@gmail.com>
2020-04-13 20:07:59 +0300

    Popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 $(function() {
 	//= include/default
 	//= include/popup
-  //= include/forms
-	
+  
+  $('.js-form-registr').on('focus', function() {
+    $(this).parent().addClass('active')
+  })
+
+  $('.js-form-registr').on('blur', function() {
+    if ($(this).val() === '') {
+      $(this).parent().removeClass('active')
+    }
+  })
+
 });
 
 
