3e75b959ff8aaa4c63a39ffda46e2eaca05b7d14
Khanh <Khanh13Nguyen13@gmail.com>
2020-04-13 20:18:55 +0300

    Revert "Popup"
    This reverts commit 92237b3705c2082d0640554d3233d7dccd4ee522.

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,8 @@
 $(function() {
 	//= include/default
 	//= include/popup
-  
-  $('.js-form-registr').on('focus', function() {
-    $(this).parent().addClass('active')
-  })
-
-  $('.js-form-registr').on('blur', function() {
-    if ($(this).val() === '') {
-      $(this).parent().removeClass('active')
-    }
-  })
-
+  //= include/forms
+	
 });
 
 
